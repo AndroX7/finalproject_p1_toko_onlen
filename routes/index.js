cec7dbc58c51a862a23085e293855c1a11fcea96
claudiovivaldi12 <claudiovivaldi389@gmail.com>
2020-08-28 01:25:30 +0700

    7th

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-// const homeRoutes = require('./home-routes.js')
-// const userRoutes = require('./user-routes.js')
+const homeRoutes = require('./home-routes.js')
+const userRoutes = require('./user-routes.js')
 const featuresRoutes = require('./features-routes.js')
 
-// router.use('/',homeRoutes)
+router.use('/',homeRoutes)
 router.use('/features',featuresRoutes)
-// router.use('/user',userRoutes)
+router.use('/user',userRoutes)
 
 module.exports = router
